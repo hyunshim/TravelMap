830164cbfbb926ac1736b61d3aa9a8a297db21ef
Hyun <hhyunshim@gmail.com>
2019-12-11 00:03:58 +1100

    Add navigation component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
 
+import NavigationBar from './components/navigation/NavigationBar';
+
 function App() {
   return (
     <div class="app">
-      Hi
+      <NavigationBar></NavigationBar>
     </div>
   );
 }
