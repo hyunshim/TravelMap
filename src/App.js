f591159ebe65f2cf4e0fa351f4b3d8740f68d5d4
Hyun <hhyunshim@gmail.com>
2019-12-11 00:48:12 +1100

    Create base map component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.scss';
 
 import NavigationBar from './components/navigation/NavigationBar';
+import BaseMap from './components/base_map/BaseMap';
 
 function App() {
   return (
     <div class="app">
       <NavigationBar></NavigationBar>
+      <BaseMap></BaseMap>
     </div>
   );
 }
---
+++ src/components/base_map/BaseMap.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './BaseMap.scss';
+
+class BaseMap extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div class="base-map">
+                I've made you a playground to work with your map :)
+            </div>
+        )
+    }
+}
+
+export default BaseMap;
