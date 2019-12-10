a5e8dbb391db44693ad5b04f8694d49a04753830
Hyun <hhyunshim@gmail.com>
2019-12-11 00:03:58 +1100

    Add navigation component

---
+++ src/components/navigation/NavigationBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NavigationBar.scss';
+
+class NavigationBar extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div class="nav">
+            </div>
+        )
+    }
+}
+
+export default NavigationBar;
