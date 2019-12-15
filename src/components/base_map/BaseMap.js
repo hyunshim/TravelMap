02f34601259f3ba1b75e50cff479c43b3698cb2f
Hyun <hhyunshim@gmail.com>
2019-12-15 22:24:00 +1100

    Add geojson files

--- src/components/base_map/BaseMap.js
+++ src/components/base_map/BaseMap.js
@@ -2,12 +2,8 @@ import React from "react";
 import "./BaseMap.scss";
 import {
   Map,
-  Marker,
-  Popup,
   TileLayer,
   GeoJSON,
-  FeatureGroup,
-  Polygon
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import GeoMap from "./geo.json";
@@ -19,22 +15,38 @@ class BaseMap extends React.Component {
     this.state = {
       lat: -37.813629,
       lng: 144.963058,
-      zoom: 13
+      zoom: 5
     };
   }
+
+  handleClick(e) {
+    console.log("e", e.latlng);
+    console.log(GeoMap.features);
+  }
+
   render() {
     const position = [this.state.lat, this.state.lng];
-    console.log(GeoMap.features[0].geometry.coordinates)
+    const rc = () => Math.round(Math.random() * 255);
+
     return (
       <div className="base-map">
         <Map
           style={{ height: "100%", width: "100%" }}
           zoom={this.state.zoom}
           center={position}
+          onClick={this.handleClick}
         >
           <TileLayer url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          <GeoJSON data={GeoMap} />
-          <Polygon color="purple" positions={GeoMap.features[0].geometry.coordinates[1]} />
+
+          <GeoJSON
+            data={GeoMap}
+            style={() => ({
+              color: "black",
+              weight: 0.5,
+              fillColor: `rgb(${rc()}, ${rc()}, ${rc()})`,
+              fillOpacity: 0.2
+            })}
+          />
         </Map>
       </div>
     );
